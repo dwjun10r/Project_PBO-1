f95558b048a2a9d83e42afd0351fe9f9be6f39f5
Duelisgood <adyananaraya605@gmail.com>
2025-04-16 23:59:59 +0800

    New File

---
+++ Login/NewLogin.java
@@ -0,0 +1,11 @@
+package login;
+
+import java.util.Scanner;
+
+public class NewLogin {
+    public void login() {
+        // Logika untuk login
+        System.out.println("Login Berhasil!");
+        // Bisa tambah validasi login di sini
+    }
+
